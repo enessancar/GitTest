e24ba6cc74f53e71e9f57806bb30755c9abfb085
enessancar <enes57751907@gmail.com>
2023-09-01 12:06:11 +0300

    Initial Commit

---
+++ SourceControl/SourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SourceControlApp.swift
+//  SourceControl
+//
+//  Created by Enes Sancar on 1.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct SourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
